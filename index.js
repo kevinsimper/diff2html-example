f6838106e362152d452d2eb085bad6dc621bf6af
Kevin Simper <kevin.simper@gmail.com>
2016-09-17 10:58:57 +0200

    Load it in the front-end instead with fetch

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let dif2html = require("diff2html").Diff2Html
-let fs = require('fs')
-let diffString = fs.readFileSync('./test.diff', 'utf8')
-var html = dif2html.getPrettyHtml(diffString);
-document.getElementById('app').innerHTML = html
-console.log(html)
+fetch('https://crossorigin.me/https://github.com/kevinsimper/hyper-dashboard/commit/7295644edee5ab8d340988ae7d128422de8e83b4.diff')
+  .then((res) => res.text())
+  .then((data) => {
+    var html = dif2html.getPrettyHtml(data);
+    document.getElementById('app').innerHTML = html
+    console.log(html)
+  })
